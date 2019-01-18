b61d32725e86aff0df7171fb7e3f29803a33c80e
Bogár László <bogarlaszlo95@gmail.com>
2019-01-18 16:34:49 +0100

    diamond

--- week-01/day-3/30 DrawDiamond/main.cpp
+++ week-01/day-3/30 DrawDiamond/main.cpp
@@ -19,25 +19,21 @@ int main() {
     std::cout<<"diamond for a number? " <<std::endl << "Number: ";
     std::cin>> a;
     std::cout <<"Diamond:" <<std::endl;
-    int space = a - 1;
-    for(int i = 1; i <=a/2+1; i++){
-        for (int c = 0; c<=space; c++){
-            std::cout<<" ";
-            space--;}
-        for (int u = 1; u<=2 * i - 1 ; u++){
-            std::cout<<"*";
-            }
-        std::cout<< std::endl;}
-    for (int i = 1; i <=a/2; i++) {
-        space = 1;
-        for (int c = i; c>=space; c--){
-            std::cout<<" ";
-            space++;}
-        for (int u = a; u>=2 * i ; u--){
-            std::cout<<"*";}
+    for (int i = 1; i<=a; i++){
+        for (int u =1; u<a-i+1; u++) {
+            std::cout <<" ";
+        }for (int k = 1; k<i*2; k++) {
+            std::cout << "*";
 
+        }std::cout<<std::endl;
+    }
+    for (int i = 1; i<=a-1; i++){
+        for (int u = a-1; u>a-i-1; u--) {
+            std::cout <<" ";
+        }for (int k = a*2-1; k>i*2; k--) {
+            std::cout << "*";
 
-     std::cout<< std::endl;
+        }std::cout<<std::endl;
     }
     return 0;
 }
