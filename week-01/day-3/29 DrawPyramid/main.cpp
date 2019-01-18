06d1df7add86fd72e8eb0f27c44b2ed1694e3a84
Bogár László <bogarlaszlo95@gmail.com>
2019-01-18 16:11:38 +0100

    pyramid change

--- week-01/day-3/29 DrawPyramid/main.cpp
+++ week-01/day-3/29 DrawPyramid/main.cpp
@@ -19,10 +19,9 @@ int main() {
     for (int i = 1; i<=a; i++){
         for (int u =1; u<a-i+1; u++) {
             std::cout <<" ";
-        }for (int k = 1; k<i*2; k++){
-            std::cout<<"*";
-        }for (int u =1; u<a-i+1; u++) {
-            std::cout <<" ";
+        }for (int k = 1; k<i*2; k++) {
+            std::cout << "*";
+
         }std::cout<<std::endl;
     }
     return 0;
--- week-01/day-3/30 DrawDiamond/main.cpp
+++ week-01/day-3/30 DrawDiamond/main.cpp
@@ -16,10 +16,28 @@ int main() {
     // The diamond should have as many lines as the number was
 
     int a;
-    std::cout<<"diamond for a number? " <<std::endl <<std::cout<<"Number: ";
+    std::cout<<"diamond for a number? " <<std::endl << "Number: ";
     std::cin>> a;
     std::cout <<"Diamond:" <<std::endl;
+    int space = a - 1;
+    for(int i = 1; i <=a/2+1; i++){
+        for (int c = 0; c<=space; c++){
+            std::cout<<" ";
+            space--;}
+        for (int u = 1; u<=2 * i - 1 ; u++){
+            std::cout<<"*";
+            }
+        std::cout<< std::endl;}
+    for (int i = 1; i <=a/2; i++) {
+        space = 1;
+        for (int c = i; c>=space; c--){
+            std::cout<<" ";
+            space++;}
+        for (int u = a; u>=2 * i ; u--){
+            std::cout<<"*";}
 
 
+     std::cout<< std::endl;
+    }
     return 0;
 }
