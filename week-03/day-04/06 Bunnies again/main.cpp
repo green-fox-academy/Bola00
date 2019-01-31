39d6b8af174b6549ded9de5275816f3e9ca6fa92
Bogár László <bogarlaszlo95@gmail.com>
2019-01-31 13:49:35 +0100

    Bunnies

--- week-03/day-04/06 Bunnies again/main.cpp
+++ week-03/day-04/06 Bunnies again/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
-// We have a number of bunnies and each bunny has two big floppy ears.
-// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
 int earsCounter (int n){
     if (n == 0){
         return 0;
-    } else{
+    }
+    else if(n % 2 == 1){
         return ( 2 + earsCounter(n-1) );
+    }
+    else{
+        return ( 3 + earsCounter(n-1) );
     }
 }
 
