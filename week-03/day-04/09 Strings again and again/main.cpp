d9b25a1e4543952c6a6e883923c5eec2b27869f7
Bogár László <bogarlaszlo95@gmail.com>
2019-01-31 15:39:22 +0100

    09 Strings again and again

--- week-03/day-04/09 Strings again and again/main.cpp
+++ week-03/day-04/09 Strings again and again/main.cpp
@@ -5,10 +5,10 @@
 // adjacent chars are now separated by a '*'.
 
 std::string stringManipulator (std::string a){
-    if (a.size() == 1){// if the last letter is 'x', delete it
+    if (a.size() == 1){
         return a;
     }else {
-        std::string first_letter = a.substr(0,1); // delete the first letter if it's an 'x'
+        std::string first_letter = a.substr(0,1); 
             first_letter.push_back('*');
         return (first_letter + stringManipulator(  a.substr(1, a.size() )  ) );
     }
