f70757ba331aa9cc7efc8508e6014b5fa91ddd00
Bogár László <bogarlaszlo95@gmail.com>
2019-02-27 15:36:51 +0100

    remake

--- week-06/day-03/08 - Percentile/main.c
+++ week-06/day-03/08 - Percentile/main.c
@@ -6,34 +6,56 @@
 //Create a function which can calculate the n-th percentile of the elements.
 //Calculate the 80-th and the 90-th percentile.
 
-int percent_calculator(int array[], int longness, int percentage)
+float percent_calculator(int *array[], int longness[2], int percentage)
 {
     int sum = 0;
-    for(int i = 0; i < longness; i++)
-    {
-        sum += array[i];
+    for (int i = 0; i < longness[0]; i++) {
+        for (int j = 0; j < longness[1]; j++) {
+            printf("%d, ",array[i][j]);
+            sum += array[i][j];
+
+        }
+        printf("\n");
     }
 
-    return sum / 100 * percentage;
+    return (float) sum / 100 * percentage;
 }
 
 int main()
 {
-    int numbercounter;
-    printf("How many numbers do you want?");
-    scanf("%d", &numbercounter);
 
-    int *numbers = NULL;
-    numbers = (int *) malloc(numbercounter * (sizeof(int)));
+    int numbercounter[2];
 
-    for (int i = 0; i < numbercounter; ++i) {
-        printf("Number %i: ", i + 1);
-        scanf("%i", &numbers[i]);
+    printf("How many lines do you want?");
+    scanf("%d", &numbercounter[0]);
+    printf("How many numbers in the lines do you want?");
+    scanf("%d", &numbercounter[1]);
+
+    int* numbers[numbercounter[0]];
+
+    for (int j = 0; j < numbercounter[0]; j++) {
+        numbers[j] = (int *) calloc(numbercounter[1], sizeof(int));
     }
 
-    printf("%d\n",percent_calculator(numbers, numbercounter, 80));
-    printf("%d\n",percent_calculator(numbers, numbercounter, 90));
+    /* int *numbers = NULL;
+     numbers = (int*) malloc(numbercounter[0] * numbercounter[1] * (sizeof(int)));
+ */
+    int counter = 1;
+    for (int j = 0; j < numbercounter[0]; ++j) {
+        for (int i = 0; i < numbercounter[1]; ++i) {
+            printf("Number %d: ", counter);
+            scanf("%i", &numbers[j][i]);
+            counter++;
+        }
+    }
 
-    free(numbers);
+    printf("%.1f\n", percent_calculator(numbers, numbercounter, 80));
+    printf("%.1f\n", percent_calculator(numbers, numbercounter, 90));
+
+    for (int k = 0; k < numbercounter[0]; ++k) {
+        free(numbers[k]);
+
+    }
+
     return 0;
 }
